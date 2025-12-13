8704690568260ba905b5bd5d3bd1beac0e6f5761
Marla Hoggard <marla@switchboard.app>
2025-12-13 01:22:42 -0500

    Source should not be required

--- src/models/recipe.ts
+++ src/models/recipe.ts
@@ -14,7 +14,7 @@ export interface IRecipe {
   _id: Types.ObjectId;
   id: number; // TODO: Decide what to do about this
   title: string;
-  source: string;
+  source: string | null;
   source_url: string | null;
   submitted_by: string;
   servings: string | number; // TODO: Currently strings, should be number?
@@ -30,8 +30,8 @@ export interface IRecipe {
 
 const RecipeSchema = new Schema<IRecipe>({
   id: { type: Number, required: true, unique: true }, // TODO: Decide what to do about this
-  title: { type: String, required: true },
-  source: { type: String, required: true },
+  title: { type: String, required: true, default: null },
+  source: { type: String, default: null },
   source_url: { type: String, required: false },
   submitted_by: { type: String, required: true },
   servings: { type: Schema.Types.Mixed, required: true }, // TODO: Currently strings, should be number?
